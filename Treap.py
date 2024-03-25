252ada9c3bab44b78192a559277f51775f7badcb
Anjaneya <anjaneyak10@gmail.com>
2024-03-25 17:13:21 -0400

    Refactoring the code

--- Treap.py
+++ Treap.py
@@ -1,5 +1,6 @@
 import random
 import time
+import matplotlib.pyplot as plt
 
 # Treap Class
 # This class has the following attributes:
@@ -34,15 +35,6 @@ class Treap:
                 f"Parent Key: {parent_key}, Parent Priority: {parent_priority}, "
                 f"Left Child Key: {left_child_key}, Left Child Priority: {left_child_priority}, "
                 f"Right Child Key: {right_child_key}, Right Child Priority: {right_child_priority}")
-
-# in_order_traversal function
-# This function accepts the root node as an argument and prints the keys and priorities in an in-order traversal of the tree
-def in_order_traversal(root):
-    if root is not None:
-        in_order_traversal(root.left)
-        print(root)
-        in_order_traversal(root.right)
-
 # left_rotate function
 # This function accepts a node as an argument and performs a left rotation on the node
 def left_rotate(tnode):
@@ -127,6 +119,7 @@ def insert(root,newNode):
         root = root.parent
     return root
 
+
 # search function
 # This function accepts the root node and a key as arguments and searches for the key in the tree
 # The function returns True if the key is found and False otherwise
@@ -139,13 +132,38 @@ def search(root, key):
         return search(root.left, key)
     return search(root.right, key)
 
-# x = insert(None, Treap('a'))
-# a= insert(x, Treap('b'))
-# z = insert(a, Treap('c'))
-# y = insert(z, Treap('d'))
-# in_order_traversal(y)
-# print(search(y, 'a'))
-# print(search(y, 'e'))
+def in_order_traversal(root):
+    if root is not None:
+        in_order_traversal(root.left)
+        print(root)
+        in_order_traversal(root.right)
+
+
+# visualize_tree function
+# This function accepts the root node of a tree and generates a DOT representation of the tree
+# The function prints the DOT representation to the console
+def visualize_tree(root):
+    dot = 'digraph tree {\nnode [shape=circle, fontname="Arial", fontsize=10];\n'
+
+    def add_node(node, label):
+        nonlocal dot
+        dot += f'{node.key}{node.priority}[label="{label}"];\n'
+        if not node.left and not node.right:
+            return
+        if not node.left:
+            dot+=f'{node.key}{node.priority} -> {node.key}LNE;\n'
+        for child in [node.left, node.right]:
+            if child:
+                dot += f'{node.key}{node.priority} -> {child.key}{child.priority};\n'
+                add_node(child, "Key = "+child.key+" Priority ="+str(child.priority))
+        if not node.right:
+            dot+=f'{node.key}{node.priority} -> {node.key}RNE;\n'
+    add_node(root, "Key = "+root.key+" Priority ="+str(root.priority))
+    dot += '}\n'
+
+    print(dot)
+
+
 
 # question3 function
 # This function creates a treap from the input list and performs an in-order traversal of the tree
@@ -154,11 +172,12 @@ def question3():
     root = None
     for i in treapInputList:
         root = insert(root, Treap(i))
-    in_order_traversal(root)
+    visualize_tree(root)
     return root
 
-
-def question4(rootNode):
+# searchInTextFile function
+# This function reads the text file 'FellowshipOfTheRing.txt' and searches for the characters in the tree
+def searchInTextFile(rootNode):
     with open('FellowshipOfTheRing.txt', 'r') as file:
         found=0
         notFound=0
@@ -168,19 +187,71 @@ def question4(rootNode):
                 if letter.isalpha():
                     if letter.islower():
                         letter = letter.upper()
-                    if not search(rootNode, letter):
-                        notFound+=1
-                        s.add(letter)
-                    else:
-                        found+=1
+                    search(rootNode, letter)
 
 
+# This function calculates the time for searching the text file
+def calculateTimeForSearch(root):
+    times = []
+    for i in range(10):
+        start = time.time()
+        searchInTextFile(root)
+        end = time.time()
+        times.append(end-start)
+    print(f'Times: {times}')
+    print(f'Average Time: {sum(times)/len(times)}')
 
-times = []
-for i in range(10):
+# This function creates a tree with given priority
+# Corresponds to q5
+def createTreeWithGivenPriority():
+    characters = ['A','B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U','V','W','X','Y','Z']
+    priorities = [24,7,14,17,26,10,8,18,22,4,5,16,13,19,23,12,2,20,21,25,15,6,11,3,9,1]
+    treapInputList =['Z','Y','X','W','V','B','U','G','M','R','K','J','D','Q','E','C','S','I','H','P','L','A','N','O','T','F']
+    map = {}
+    for i in range(len(characters)):
+        map[characters[i]] = priorities[i]
+    root = None
+    for i in treapInputList:
+        root = insert(root, Treap(i, map[i]))
+    visualize_tree(root)
+    return root
+
+
+# This function creates a tree with same priority
+# Corresponds to q6
+def createTreeWithSamePriority():
+    characters =['Z','Y','X','W','V','B','U','G','M','R','K','J','D','Q','E','C','S','I','H','P','L','A','N','O','T','F']
+    root = None
+    for i in range(len(characters)):
+        root = insert(root, Treap(characters[i],1))
+    visualize_tree(root)
+    return root
+
+# main function
+# This function creates a treap and inserts nodes into it
+def main():
+    # Verifying the values
     root = question3()
-    start = time.time()
-    question4(root)
-    end = time.time()
-    times.append(end-start)
-print(f'Times: {times}', f'Average Time: {sum(times)/len(times)}')
+    # in_order_traversal(root)
+
+    # Calculating the time for searching the text file Q4
+    calculateTimeForSearch(root)
+
+    print()
+    print("Creating a tree with given priority and calculting the time for searching the text file")
+    root = createTreeWithGivenPriority()
+    calculateTimeForSearch(root)
+
+
+    print()
+    print("Creating a tree with same priority and calculting the time for searching the text file")
+    root = createTreeWithSamePriority()
+    calculateTimeForSearch(root)
+
+
+main()
+
+
+
+
+
